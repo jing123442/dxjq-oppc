8a02ad446b7484ab21f246eef6f1c656732748d0
王贵旺 <ywangxin928@163.com>
2020-12-16 09:52:28 +0800

    bug

--- src/router/order.js
+++ src/router/order.js
@@ -3,45 +3,16 @@ const inImport = process.env.NODE_ENV == 'prod' ? require('./_product') : requir
 
 export default [
   {
-    path: '/filler',
+    path: '/order',
+    name: 'order',
     component: Layout,
-    redirect: '/filler/index',
-    meta: { title: '加气站管理' },
+    redirect: '/order/index',
+    meta: { title: '订单中心' },
     children: [
-      { path: 'index', component: inImport('filler/index'), name: 'filler', meta: { title: '加气站列表', icon: 'icon', noCache: false } },
-      { path: 'fillerInfo', component: inImport('filler/fillerInfo'), name: 'fillerInfo', meta: { title: '加气站信息维护', icon: 'icon-', noCache: false } },
-      { path: 'fillerAccount', component: inImport('filler/account'), name: 'fillerAccount', meta: { title: '加气站资金账户管理', icon: 'icon-gongnengguanli', noCache: false } },
-      { path: 'fillerPrice', component: inImport('filler/price'), name: 'fillerPrice', meta: { title: '加气站挂牌价查询', icon: 'icon-gongnengguanli', noCache: false } },
-      { path: 'gasStockList', component: inImport('filler/gasStockList'), name: 'gasStockList', meta: { title: '加气站库存管理', icon: 'icon-gongnengguanli', noCache: false } },
-      { path: 'lngPlan', component: inImport('filler/lngPlan'), name: 'lngPlan', meta: { title: 'LNG计划管理', icon: 'icon-gongnengguanli', noCache: false } },
-      {
-        path: 'fillerInfo',
-        component: { render (c) { return c('router-view') } },
-        hidden: true,
-        meta: { title: '加气站列表', icon: 'icon-gongnengguanli', noCache: false },
-        children: [
-          { path: 'printerList', component: inImport('filler/sub/printerList'), name: 'printerList', meta: { title: '打印机列表', icon: 'icon-gongnengguanli', noCache: false } }
-        ]
-      },
-      {
-        path: 'fillerAccount',
-        component: { render (c) { return c('router-view') } },
-        hidden: true,
-        meta: { title: '加气站资金账户管理', icon: 'icon-gongnengguanli', noCache: false },
-        children: [
-          { path: 'accountList', component: inImport('filler/sub/accountList'), name: 'accountFillerList', meta: { title: '流水列表', icon: 'icon-gongnengguanli', noCache: false } }
-        ]
-      },
-      {
-        path: 'fillerPrice',
-        component: { render (c) { return c('router-view') } },
-        name: 'lngServiceFeeSetting',
-        hidden: true,
-        meta: { title: '加气站挂牌价查询', icon: 'icon-gongnengguanli', noCache: false },
-        children: [
-          { path: 'serviceChangeRecord', component: inImport('filler/sub/serviceChangeRecord'), name: 'serviceChangeRecord', meta: { title: '服务费变更记录', icon: 'icon-gongnengguanli', noCache: false } }
-        ]
-      }
+      { path: 'orderIndex', component: inImport('order/index'), name: 'orderIndex', meta: { title: '加气订单', noCache: false } },
+      { path: 'orderFiller', component: inImport('order/filler'), name: 'orderFiller', meta: { title: '加气站结算订单', noCache: false } },
+      { path: 'orderCarrier', component: inImport('order/carrier'), name: 'orderCarrier', meta: { title: '物流公司结算订单', noCache: false } },
+      { path: 'orderBusiness', component: inImport('order/business'), name: 'orderBusiness', meta: { title: '象群科技结算订单', noCache: false } }
     ]
   }
 ]
