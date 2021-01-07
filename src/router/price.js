e2c84fd907fa1f6345f335f6d383f4e3d55937a9
王贵旺 <ywangxin928@163.com>
2021-01-07 08:56:22 +0800

    bug

--- src/router/price.js
+++ src/router/price.js
@@ -24,7 +24,7 @@ export default [
           { path: 'departure', component: inImport('price/sub/departure'), name: 'departure', meta: { title: '长城奥扬出港价', icon: 'icon-gongnengguanli', noCache: false } },
           { path: 'preferential', component: inImport('price/sub/preferential'), name: 'preferential', meta: { title: '区域优惠政策', icon: 'icon-gongnengguanli', noCache: false } },
           { path: 'estimate', component: inImport('price/sub/estimate'), name: 'estimate', meta: { title: '运费测算', icon: 'icon-gongnengguanli', noCache: false } },
-          { path: 'freight', component: inImport('price/sub/freight'), name: 'freight', meta: { title: '配置价格', icon: 'icon-gongnengguanli', noCache: false } },
+          { path: 'freight', component: inImport('price/sub/freight'), name: 'freight', meta: { title: '运费单价', icon: 'icon-gongnengguanli', noCache: false } },
           { path: 'measure', component: inImport('price/sub/measure'), name: 'measure', meta: { title: '气价调节', icon: 'icon-gongnengguanli', noCache: false } },
           { path: 'mileage', component: inImport('price/sub/mileage'), name: 'mileage', meta: { title: '加气站里程', icon: 'icon-gongnengguanli', noCache: false } }
         ]
