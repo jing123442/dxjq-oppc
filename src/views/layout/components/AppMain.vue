f7408f572e4cf9823f41658513c4bf9b2f12bcc5
王贵旺 <ywangxin928@163.com>
2021-05-12 15:38:52 +0800

    修改layout布局，并修改css样式

--- src/views/layout/components/AppMain.vue
+++ src/views/layout/components/AppMain.vue
@@ -1,23 +1,24 @@
 <template>
   <section class="app-main">
-    <scroll-bar :isResetTop="true">
-      <transition name="fade" mode="out-in">
-        <keep-alive :include="cachedViews">
-          <router-view></router-view>
-        </keep-alive>
-      </transition>
-    </scroll-bar>
+    <transition name="fade" mode="out-in">
+      <keep-alive :include="cachedViews">
+        <router-view :key="key"></router-view>
+      </keep-alive>
+    </transition>
   </section>
 </template>
 <script>
-import ScrollBar from '@/components/ScrollBar'
+// import ScrollBar from '@/components/ScrollBar'
 
 export default {
   name: 'AppMain',
-  components: { ScrollBar },
+  // components: { ScrollBar },
   computed: {
     cachedViews() {
       return this.$store.state.tagsView.cachedViews
+    },
+    key() {
+      return this.$route.fullPath
     }
   }
 }
