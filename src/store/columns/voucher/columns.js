f7a1376dd1543945510bd33cbd11a979dda56145
王贵旺 <ywangxin928@163.com>
2021-06-29 09:48:20 +0800

    修改菜单和按钮信息

--- src/store/columns/voucher/columns.js
+++ src/store/columns/voucher/columns.js
@@ -12,7 +12,7 @@ const columns = {
     { field: 'certificateNumber', name: '凭证号', search: { type: 'select', name: '企业名称', remote: true, field: 'orgId', obj: 'orgId', findKey: 'orgName', paramKey: ['param', 'org'], placeholder: '企业名称', required: true } },
   ],
   list: [
-    { field: 'serialNumber', name: '流水号', search: { type: 'input-range', columns: [{ type: 'text' }, { type: 'text' }]} },
+    { field: 'serialNumber', name: '流水号' },
     { field: 'changeTime', name: '记账时间', formatFun: 'formateTData all', width: 140, stype: 'format', search: { type: 'date-picker', model: 'daterange', dataType: 'rangeType', rangeNumber: 31, style: 'width: 350px;', format: 'yyyy-MM-dd', timeOptions: toolPickerOptions(0), value: (function() { const dateObj = dataPickerDefault(new Date(), 1, 'yyyy-MM-dd hh:mm:ss', 1); return [dateObj.start, dateObj.end] })() } },
     { field: 'happenTime', name: '发生时间', formatFun: 'formateTData all', width: 140, stype: 'format' },
     { field: 'summary', name: '摘要' },
@@ -36,9 +36,9 @@ const columns = {
     { field: 'changeTime', name: '期间', stype: 'fields', fieldList: ['startDate', 'endDate'], sign: ' 至 ', search: { type: 'date-picker', style: 'width: 350px;', dtime: ['00:00:00', '23:59:59'] } },
     { field: 'createTime', name: '申请时间', formatFun: 'formateTData all', stype: 'format' },
     { field: 'status', name: '状态', formatter: 'voucherStatus', search: { type: 'select', obj: 'voucherStatus', placeholder: '盖章状态' } },
-    { field: 'useropts', stype: 'opt', ispush: false, name: '操作', note: '工作日凌晨12点前申请下载的文件T+1后可下载盖章文件，需要紧急大量盖章或者异常导致的盖章失败，可下载未盖章凭证联系通联转人工处理', fixed: 'right', width: 330, list: function (scope) {
+    { field: 'useropts', stype: 'opt', ispush: false, name: '操作', note: '工作日凌晨12点前申请下载的文件T+1后可下载盖章文件，需要紧急大量盖章或者异常导致的盖章失败，可下载未盖章流水联系通联转人工处理', fixed: 'right', width: 330, list: function (scope) {
         const row = scope.row
-        const result =[{type: 'self_detail', name: '流水凭证明细'}]
+        const result =[{type: 'self_detail', name: '资金流水明细'}]
 
         if (row.noSealFilePath) {
           result.push({type: 'down_no', name: '下载未盖章流水'})
