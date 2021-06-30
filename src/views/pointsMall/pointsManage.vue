0839faf10c396cee23b426c307835b265bcae6e5
zhuchao <zhuchao1121@qq.com>
2021-06-30 16:03:18 +0800

    积分商城基本设置优化

--- src/views/pointsMall/pointsManage.vue
+++ src/views/pointsMall/pointsManage.vue
@@ -56,6 +56,7 @@ export default {
   methods: {
     onListEvent(obj) {
       if (obj.type == 'ok') {
+        if (!this.points.pointsValue) { return }
         this.editPoints(this.points)
       }
     },
