ef49047803cf1e9e194965f7bf520a9debd600e3
zhuchao <zhuchao1121@qq.com>
2022-02-19 16:03:10 +0800

    1

--- src/store/columns/business/select.js
+++ src/store/columns/business/select.js
@@ -12,8 +12,10 @@ const select = {
   },
   account: {
     accountType: [
-      { value: 0, label: '现金账户' },
-      { value: 1, label: '积分账号' }
+      { value: 0, label: '通联现金账户' },
+      { value: 1, label: '积分账号' },
+      { value: 2, label: '北京银行现金' },
+      { value: 3, label: '数字账户' }
     ],
     tradeType: utilsTradeModeType()
   },
