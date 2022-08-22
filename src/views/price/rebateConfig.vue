9d0cda234c2a49b2729839247d202332c5c507b3
houqq <641301341@qq.com>
2022-08-22 23:07:10 +0800

    fix(ui): 修改享受优惠额度负数限制

--- src/views/price/rebateConfig.vue
+++ src/views/price/rebateConfig.vue
@@ -146,10 +146,10 @@ export default {
           this.$message.error('请选择要添加的优惠加气站')
           return
         }
-        if (this.rebateFillerAddRow.rebate !== '' && this.rebateFillerAddRow.rebate <= 0) {
-          this.$message.error('享受优惠额度必须大于0')
-          return
-        }
+        // if (this.rebateFillerAddRow.rebate !== '' && this.rebateFillerAddRow.rebate <= 0) {
+        //   this.$message.error('享受优惠额度必须大于0')
+        //   return
+        // }
         const temp = []
         this.selectedList.forEach(item => {
           temp.push({
@@ -178,10 +178,10 @@ export default {
     },
     btnClickEventRebateEdit(item) {
       if (item.type == 'ok') {
-        if (this.rebateEditRow.rebate !== '' && this.rebateEditRow.rebate <= 0) {
-          this.$message.error('享受优惠额度必须大于0')
-          return
-        }
+        // if (this.rebateEditRow.rebate !== '' && this.rebateEditRow.rebate <= 0) {
+        //   this.$message.error('享受优惠额度必须大于0')
+        //   return
+        // }
         this.$refs.configEdit.$refs.rebateEditRow.validate(valid => {
           if (valid) {
             const params = {
