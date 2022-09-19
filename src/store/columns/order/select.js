d4ce5724bdc828d22fbe24b490897f17320af241
houqq <641301341@qq.com>
2022-09-19 20:50:39 +0800

    fix: 拆分筛选条件

--- src/store/columns/order/select.js
+++ src/store/columns/order/select.js
@@ -7,7 +7,8 @@ const select = {
     tradeType: utilsTradeModeType(),
     currDaySearch: [{ value: 'orderId', label: '订单编号', dataType: 'number' }, { value: 'carrierOrgName', label: '物流公司' }, { value: 'carNumber', label: '车牌号' }, { value: 'gasstationName', label: '加气站' }],
     currDataSearch: [{ value: 'createDate', label: '创建时间' }, { value: 'updateDate', label: '支付时间' }],
-    currFieldSearch: [{ value: 'carNumber', label: '车牌号' }, { value: 'orderId', label: '订单编号', dataType: 'number' }, { value: 'carrierOrgName', label: '物流公司' }, { value: 'driverName', label: '司机姓名' }, { value: 'gasstationName', label: '加气站' }, { value: 'cashierName', label: '收银员姓名' }]
+    currFieldSearch: [{ value: 'carNumber', label: '车牌号' }, { value: 'orderId', label: '订单编号', dataType: 'number' }, { value: 'driverName', label: '司机姓名' }, { value: 'cashierName', label: '收银员姓名' }],
+    carrierOrgNameSearch: [{ value: 'carrierOrgName', label: '物流公司' },]
   },
   selfOrder: {
     payType: utilsPayType(),
