799971b2149b50617df93d5d9412e6a2bdf83b38
jing123442 <3446484442@qq.com>
2024-05-01 20:29:22 +0800

    feat: 定价管理-form组件

---
+++ src/views/price/components/uploadVoucher.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+      <el-upload
+        :key="index"
+        :action="'#'"
+        class="fileBox"
+        list-type="none"
+        accept=".jpg,.png,bmp"
+        :multiple="true"
+              :limit="9"
+              :show-file-list="false"
+              :on-change="(file, fileList) => changeFile(file, fileList, index)"
+              :auto-upload="false">
+              <div slot="default">
+                <img :src="item.path ? item.path : item.defaultPath" alt="">
+              </div>
+            </el-upload>
+        </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      imgList: [{
+        field: '',
+        path: '',
+        isDisabled: false,
+        defaultPath: require('@/assets/images/main/license@2x.png'),
+        text: ''
+      }]
+    }
+  },
+  methods: {
+    changeFile(file, fileList, index) {
+      const fileD = new FormData()
+      fileD.append('file', file.raw)
+      console.log(file)
+    //   $upload(fileD).then(res => {
+    //     if (res.data.code === 0) {
+    //       var pathUrl = this.$store.state.file.fileHost + res.data.data
+    //       this.imgList[index].path = pathUrl
+    //       if (this.imgList.length < 3) {
+    //         this.$set(this.imgList, this.imgList.length, {
+    //           field: '',
+    //           path: '',
+    //           isDisabled: false,
+    //           defaultPath: require('@/assets/images/main/license@2x.png'),
+    //           text: ''
+    //         })
+    //       }
+    //     }
+    //   })
+    }
+  }
+}
+</script>
+<style>
+</style>
